c36cc788f4530ba873653fa6fc8c7a2259156174
Chirag_Baldaniya <115916869+bchiragb@users.noreply.github.com>
2023-06-14 10:52:23 +0530

    Update admin notice, msg.php

--- admin notice, msg.php
+++ admin notice, msg.php
@@ -1,19 +1,14 @@
 <?php
-//show custom msg
 function general_admin_notice(){
     global $pagenow; global $post;
-      if($pagenow == 'options-general.php'){
-          echo '<div class="notice notice-warning is-dismissible">
-          <p>This is an example of a notice that appears on the settings page.</p>
-          </div>';
-      } else if($pagenow == 'edit.php'){
-        if(!empty($post) && $post->post_type == "listing"){
-          echo '<div class="notice notice-warning is-dismissible">
-          <p>aaaaaaaaaaaaaa.</p>
-          </div>';
-        }
+	if($pagenow == 'options-general.php'){
+		echo '<div class="notice notice-warning is-dismissible"><p>This is an example of a notice that appears on the settings page.</p></div>';
+	} else if($pagenow == 'edit.php'){
+		if(!empty($post) && $post->post_type == "listing"){
+			echo '<div class="notice notice-warning is-dismissible"><p>aaaaaaaaaaaaaa.</p></div>';
+			//wp_die( 'WordPress need to be at least 6.0 to activate this plugin' );
+		}
     }
 }
 add_action('admin_notices', 'general_admin_notice');
-
 ?>

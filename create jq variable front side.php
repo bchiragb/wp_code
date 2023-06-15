ad58253899e04ef90862e0bac1e93019a0ff754f
Chirag_Baldaniya <115916869+bchiragb@users.noreply.github.com>
2023-06-15 12:09:40 +0530

    Update create jq variable front side.php

--- create jq variable front side.php
+++ create jq variable front side.php
@@ -8,4 +8,9 @@ function javascript_variables(){ ?>
 }
 add_action ( 'wp_head', 'javascript_variables' );
 
+function enqueue_scripts() {
+	wp_enqueue_script( 'custom-js', plugin_dir_url( __FILE__ ) . 'js/custom.js', array( 'jquery' ), '', true );
+	wp_enqueue_style( 'style-css', plugin_dir_url( __FILE__ ) . 'css/custom.css' );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_scripts');
 ?>
